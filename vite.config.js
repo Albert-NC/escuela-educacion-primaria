5acfbc0032d520580dc1ac7e3292d1c8b1d51351
theprince <cfcrespinro@unitru.edu.pe>
2026-05-31 18:17:30 -0500

    fix: resolver 404 en entorno de desarrollo ajustando base condicional en vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/wp-content/themes/educacion-primaria/dist/',
-})
+  base: command === 'serve' ? '/' : '/wp-content/themes/educacion-primaria/dist/',
+  build: {
+    outDir: '../wp-content/themes/educacion-primaria/dist',
+    emptyOutDir: true,
+  }
+}))
